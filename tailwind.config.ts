c2f0e74cb5f42a4b935a1cfe8708708f94191469
여인영 <marineyeong@gmail.com>
2024-06-05 17:59:57 +0900

    [chore] 초기 셋팅을 위한 tailwind.config 및 globals.css 작성

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,29 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      primary: '956BED',
+      'text-primary': '252B42',
+      'text-secondary': '737373',
+      white: 'FFFFFF',
+      muted: 'BDBDBD',
+      success: '2DC071',
+      danger: 'E74040',
+      alert: 'E77C40',
+    },
+    fontFamily: {
+      sans: ['Noto Sans KR'],
+    },
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
