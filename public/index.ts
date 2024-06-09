e074c1cb0c97d2cb8f8be92a22c3f51ca16daa3c
enjoylonelines <hobeom5022@naver.com>
2024-06-09 17:19:43 +0900

    [chore] 하단메뉴바 아이콘 이미지 추가

---
+++ public/index.ts
@@ -0,0 +1,23 @@
+import commIcon from '../public/icons/bottomMenuBar/passive/comm.png';
+import infoIcon from '../public/icons/bottomMenuBar/passive/info.png';
+import homeIcon from '../public/icons/bottomMenuBar/passive/home.png';
+import libraryIcon from '../public/icons/bottomMenuBar/passive/library.png';
+import plusIcon from '../public/icons/bottomMenuBar/passive/plus.png';
+import commActiveIcon from '../public/icons/bottomMenuBar/active/comm.png';
+import infoActiveIcon from '../public/icons/bottomMenuBar/active/info.png';
+import homeActiveIcon from '../public/icons/bottomMenuBar/active/home.png';
+import libraryActiveIcon from '../public/icons/bottomMenuBar/active/library.png';
+import plusActiveIcon from '../public/icons/bottomMenuBar/active/plus.png';
+
+export {
+  commIcon,
+  infoIcon,
+  homeIcon,
+  libraryIcon,
+  plusIcon,
+  commActiveIcon,
+  homeActiveIcon,
+  infoActiveIcon,
+  libraryActiveIcon,
+  plusActiveIcon,
+};
