1c1239104a436567bf260becba2f6431e6288268
h220101 <ssw3417@naver.com>
2024-06-10 15:15:17 +0900

    feat: 버튼 추가작업

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
   };
 
   return (
-    <main className="flex flex-col h-screen gap-2 bg-alert p-10 items-center">
+    <main className="flex flex-col h-full gap-2 bg-gray p-10 items-center">
       <Header />
 
       <CancelSaveHeader 
@@ -34,22 +34,119 @@ export default function Home() {
         onSave={handleSave}
       />
 
+      xl-full
       <Button 
-        type="default" 
+        type="xl-full" 
         onClick={handleClick}
       >
-        기본 버튼
+        로그인
       </Button>
 
+      xl-line
       <Button 
-        type="default-white" 
+        type="xl-line" 
         onClick={handleClick}
       >
-        라인 버튼
+        취소
       </Button>
 
+      xl-line-plus
       <Button 
-        type="switch" 
+        type="xl-line-plus" 
+        onClick={handleClick}
+      />
+
+      l-card
+      <Button 
+        type="l-card" 
+        onClick={handleClick}
+      >
+        카드 추가
+      </Button>
+
+      l-delete
+      <Button 
+        type="l-delete" 
+        onClick={handleClick}
+      >
+        폴더 지우기
+      </Button>
+
+      l-rewrite
+      <Button 
+        type="l-rewrite" 
+        onClick={handleClick}
+      >
+        다시 생성하기
+      </Button>
+
+      m-line
+      <div className="flex gap-2">
+        <Button 
+          type="m-line" 
+          onClick={handleClick}
+        >
+          학습 모드
+        </Button>
+        <Button 
+          type="m-line" 
+          onClick={handleClick}
+        >
+          시험 모드
+        </Button>
+      </div>
+
+      s-full
+      <Button 
+        type="s-full" 
+        onClick={handleClick}
+      >
+        추가하기
+      </Button>
+
+      xs-full
+      <Button 
+        type="xs-full" 
+        onClick={handleClick}
+      >
+        다음
+      </Button>
+
+      m-more
+      <Button 
+        type="m-more" 
+        onClick={handleClick}
+      >
+        오늘 학습하러 가기
+      </Button>
+
+      s-more
+      <Button 
+        type="s-more" 
+        onClick={handleClick}
+      >
+        더보기
+      </Button>
+
+      xs-more
+      <Button 
+        type="xs-more" 
+        onClick={handleClick}
+      >
+        더보기
+      </Button>
+
+      black
+      <Button 
+        type="black" 
+        onClick={handleClick}
+      >
+        비밀번호 재설정
+      </Button>
+
+      group
+      <Button 
+        type="group" 
         onClick={handleClick} 
         buttonContents={[
           { label: '새로 만들기' },
@@ -59,13 +156,15 @@ export default function Home() {
       />
 
       <Button 
-        type="switch" 
+        type="group" 
         onClick={handleClick} 
         buttonContents={[
           { icon: '/icons/private.png', activeIcon: '/icons/private-active.png', label: '비공개' },
           { icon: '/icons/public.png',  activeIcon: '/icons/public-active.png', label: '공개' },
         ]}
       />
+
+
     </main>
   );
 }
