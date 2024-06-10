857aea4d02d35b2270fb67fa1f1de571e2dfb3c2
enjoylonelines <hobeom5022@naver.com>
2024-06-11 01:04:53 +0900

    [chore] 스타일가이드 폴더 구조 수정

--- src/shared/@common/ui/baseFrame/BaseFrame.tsx
+++ src/shared/@common/ui/baseFrame/BaseFrame.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import BottomMenuBar from '../bottomMenu/BottomMenuBar';
+import Header from '../Header';
 
 function BaseFrame({ children }: { children: any }) {
   return (
     <main className="flex flex-col items-center justify-between min-h-screen">
-      {/* header */}
+      <Header />
       <div className="container py-6 bg-gray mx-auto flex flex-col gap-[12px] calc_h">{children}</div>
       <BottomMenuBar />
     </main>
