8de90f5a4c34cb75a3a254674372b83889be459e
enjoylonelines <hobeom5022@naver.com>
2024-06-11 16:53:48 +0900

    [chore] 하단 메뉴 페이지 라우팅

---
+++ src/app/comm/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CommunityPage = () => {
+  return <div>커뮤니티 페이지</div>;
+};
+
+export default CommunityPage;
---
+++ src/app/home/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const HomePage = () => {
+  return <div>홈 페이지</div>;
+};
+
+export default HomePage;
