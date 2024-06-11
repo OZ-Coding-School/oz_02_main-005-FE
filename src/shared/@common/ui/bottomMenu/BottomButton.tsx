acc7fd4fe8e32779b8ac7dea6199406f7a80b138
enjoylonelines <hobeom5022@naver.com>
2024-06-11 16:53:08 +0900

    [chore] 하단메뉴바 버튼 매핑 및 라우팅

--- src/shared/@common/ui/bottomMenu/BottomButton.tsx
+++ src/shared/@common/ui/bottomMenu/BottomButton.tsx
@@ -1,16 +1,17 @@
-'use client';
 import Image, { StaticImageData } from 'next/image';
-import React, { useState } from 'react';
+import React, { MouseEvent, useState } from 'react';
 
 interface IconProps {
   passiveIcon: StaticImageData;
   activeIcon: StaticImageData;
+  onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-function BottomButton({ passiveIcon, activeIcon }: IconProps) {
+function BottomButton({ passiveIcon, activeIcon, onClick }: IconProps) {
   const [isHovered, setIsHovered] = useState(false);
   return (
     <button
+      onClick={onClick}
       className="w-[2.1875rem] h-[2.1875rem]"
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}>
--- src/shared/@common/ui/bottomMenu/BottomMenuBar.tsx
+++ src/shared/@common/ui/bottomMenu/BottomMenuBar.tsx
@@ -1,3 +1,5 @@
+'use client';
+import { useRouter } from 'next/navigation';
 import {
   commActiveIcon,
   commIcon,
@@ -5,21 +7,57 @@ import {
   homeIcon,
   infoActiveIcon,
   infoIcon,
-  libraryActiveIcon,
-  libraryIcon,
+  libActiveIcon,
+  libIcon,
   plusActiveIcon,
   plusIcon,
 } from '../../../../../public/icons/bottomMenuBar';
 import BottomButton from './BottomButton';
 
+const BUTTON_PROPS = [
+  {
+    passiveIcon: homeIcon,
+    activeIcon: homeActiveIcon,
+    url: '/',
+  },
+  {
+    passiveIcon: commIcon,
+    activeIcon: commActiveIcon,
+    url: '/comm',
+  },
+  {
+    passiveIcon: plusIcon,
+    activeIcon: plusActiveIcon,
+    url: '/plus',
+  },
+  {
+    passiveIcon: libIcon,
+    activeIcon: libActiveIcon,
+    url: '/lib',
+  },
+  {
+    passiveIcon: infoIcon,
+    activeIcon: infoActiveIcon,
+    url: '/info',
+  },
+];
+
 function BottomMenuBar() {
+  const router = useRouter();
+
   return (
     <div className="w-[390px] h-[65px] bg-white flex justify-between items-center px-[1.5625rem] fixed bottom-0 z-10">
-      <BottomButton passiveIcon={homeIcon} activeIcon={homeActiveIcon} />
-      <BottomButton passiveIcon={commIcon} activeIcon={commActiveIcon} />
-      <BottomButton passiveIcon={plusIcon} activeIcon={plusActiveIcon} />
-      <BottomButton passiveIcon={libraryIcon} activeIcon={libraryActiveIcon} />
-      <BottomButton passiveIcon={infoIcon} activeIcon={infoActiveIcon} />
+      {BUTTON_PROPS.map(({ passiveIcon, activeIcon, url }) => (
+        <BottomButton
+          key={url}
+          passiveIcon={passiveIcon}
+          activeIcon={activeIcon}
+          onClick={e => {
+            e.preventDefault;
+            router.push(url);
+          }}
+        />
+      ))}
     </div>
   );
 }
