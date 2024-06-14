ad3a6959c918166db389058993ea9b031fe0bba2
enjoylonelines <hobeom5022@naver.com>
2024-06-15 08:32:24 +0900

    [feat] 폴더 리스트 페이지 구현

---
+++ src/app/lib/folders/page.tsx
@@ -0,0 +1,18 @@
+'use client';
+import React from 'react';
+import FolderListItem from '@/shared/library/FolderListItem';
+import Button from '@/shared/@common/ui/Button/Button';
+
+function handlePlusButtonClick() {}
+
+const LibraryFoldersPage = () => {
+  return (
+    <>
+      <FolderListItem count={2} title="영어 회화" />
+      <FolderListItem count={2} title="영어 회화" />
+      <Button type="xl-line-plus" onClick={handlePlusButtonClick} />
+    </>
+  );
+};
+
+export default LibraryFoldersPage;
