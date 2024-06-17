44184fa89b4995052ca6331259d71122fc18169f
enjoylonelines <hobeom5022@naver.com>
2024-06-17 18:57:19 +0900

    [chore] 커스텀 색상 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,7 @@ const config: Config = {
       white: '#FFFFFF',
       muted: '#BDBDBD',
       success: '#2DC071',
+      successlight: '#F2FFF7',
       danger: '#E74040',
       dangerlight: '#FFDEDE',
       dangergray: '#DCBCBC',
