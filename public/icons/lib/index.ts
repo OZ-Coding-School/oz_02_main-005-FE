02fc2ea5bc15be18e81bd7dd807fa7db64fdc44c
enjoylonelines <hobeom5022@naver.com>
2024-06-17 18:58:16 +0900

    [chore] 아이콘 추가

--- public/icons/lib/index.ts
+++ public/icons/lib/index.ts
@@ -1,3 +1,5 @@
 import FolderIcon from '../../../public/icons/lib/folder.svg';
 import EditIcon from '../../../public/icons/lib/edit.svg';
-export { EditIcon, FolderIcon };
+import SuccessIcon from '../../../public/icons/lib/success.svg';
+import DangerIcon from '../../../public/icons/lib/danger.svg';
+export { EditIcon, FolderIcon, SuccessIcon, DangerIcon };
